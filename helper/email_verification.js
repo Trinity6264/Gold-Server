b747e8f5e14b7f6da47e43a344831bc8a50b796f
Trinity <amoahtnt@gmail.com>
2022-10-02 01:10:06 +0000

    Refactor

--- helper/email_verification.js
+++ helper/email_verification.js
@@ -7,8 +7,8 @@ config();
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: process.env.AUTH_EMAIL,
-    pass: process.env.AUTH_PASSWORD,
+    user: 'autumshipmentalics@gmail.com',
+    pass: 'omjogburvgzwxxyv',
   },
 });
 
